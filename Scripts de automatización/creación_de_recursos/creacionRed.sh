9ab10fc001c2c829a45fd59ccb7acd9185d32238
hypherion78 <169054574+hypherion78@users.noreply.github.com>
2024-05-27 13:22:20 +0200

    Update creacionRed.sh

--- Scripts de automatización/creación_de_recursos/creacionRed.sh
+++ Scripts de automatización/creación_de_recursos/creacionRed.sh
@@ -18,7 +18,7 @@ openstack network create \
 # Creamos la subred para la red externa. Se asignará a la red externa en el router  Cuando se configure con "openstack router set"
 openstack subnet create \
   --network red-externa-equipos \
-  --allocation-pool start=192.168.22.248,end=192.168.22.254 \
+  --allocation-pool start=192.168.22.180,end=192.168.22.190 \
   --dns-nameserver 192.168.22.100 \
   --gateway 192.168.22.100 \
   --subnet-range 192.168.22.0/24 \
